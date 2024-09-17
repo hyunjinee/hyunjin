761cda03716b658810a8458a31110821f5273148
hyunjin <leehj0110@kakao.com>
2024-09-17 13:07:18 +0900

    feat: Theme Toggle

--- services/blog/src/app/page.tsx
+++ services/blog/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { ThemeToggle } from '~/components/theme-toggle';
+
 const Page = () => {
   return (
     <div>
       <h1 className="size-30 text-4xl font-bold">Blog</h1>
+
+      <ThemeToggle />
     </div>
   );
 };
