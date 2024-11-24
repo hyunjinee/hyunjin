e6089be0d1ce13d94574bd072a8f1aae0dcd6a06
hyunjin <leehj0110@kakao.com>
2024-11-24 23:11:08 +0900

    feat: next 15

--- services/blog/app/blog/page/[page]/page.tsx
+++ services/blog/app/blog/page/[page]/page.tsx
@@ -1,34 +1,27 @@
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
+import ListLayout from '@/layouts/ListLayoutWithTags';
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer';
+import { allBlogs } from 'contentlayer/generated';
 
-const POSTS_PER_PAGE = 5
+const POSTS_PER_PAGE = 5;
 
 export const generateStaticParams = async () => {
-  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
-  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE);
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }));
 
-  return paths
-}
+  return paths;
+};
 
-export default function Page({ params }: { params: { page: string } }) {
-  const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = parseInt(params.page as string)
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
+export default async function Page({ params }: { params: Promise<{ page: string }> }) {
+  const test = await params;
+  const posts = allCoreContent(sortPosts(allBlogs));
+  const pageNumber = parseInt(test.page as string);
+  const initialDisplayPosts = posts.slice(POSTS_PER_PAGE * (pageNumber - 1), POSTS_PER_PAGE * pageNumber);
   const pagination = {
     currentPage: pageNumber,
     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+  };
 
   return (
-    <ListLayout
-      posts={posts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title="All Posts"
-    />
-  )
+    <ListLayout posts={posts} initialDisplayPosts={initialDisplayPosts} pagination={pagination} title="All Posts" />
+  );
 }
--- services/blog/app/tags/[tag]/page.tsx
+++ services/blog/app/tags/[tag]/page.tsx
@@ -1,15 +1,16 @@
-import { slug } from 'github-slugger'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allBlogs } from 'contentlayer/generated'
-import tagData from 'app/tag-data.json'
-import { genPageMetadata } from 'app/seo'
-import { Metadata } from 'next'
-import { notFound } from 'next/navigation'
+import { slug } from 'github-slugger';
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer';
+import siteMetadata from '@/data/siteMetadata';
+import ListLayout from '@/layouts/ListLayoutWithTags';
+import { allBlogs } from 'contentlayer/generated';
+import tagData from 'app/tag-data.json';
+import { genPageMetadata } from 'app/seo';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
-  const tag = decodeURI(params.tag)
+export async function generateMetadata({ params }: { params: Promise<{ tag: string }> }): Promise<Metadata> {
+  const test = await params;
+  const tag = decodeURI(test.tag);
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
@@ -19,27 +20,28 @@ export async function generateMetadata({ params }: { params: { tag: string } }):
         'application/rss+xml': `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`,
       },
     },
-  })
+  });
 }
 
 export const generateStaticParams = async () => {
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
+  const tagCounts = tagData as Record<string, number>;
+  const tagKeys = Object.keys(tagCounts);
   const paths = tagKeys.map((tag) => ({
     tag: encodeURI(tag),
-  }))
-  return paths
-}
+  }));
+  return paths;
+};
 
-export default function TagPage({ params }: { params: { tag: string } }) {
-  const tag = decodeURI(params.tag)
+export default async function TagPage({ params }: { params: Promise<{ tag: string }> }) {
+  const test = await params;
+  const tag = decodeURI(test.tag);
   // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1);
   const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
-  )
+    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag))),
+  );
   if (filteredPosts.length === 0) {
-    return notFound()
+    return notFound();
   }
-  return <ListLayout posts={filteredPosts} title={title} />
+  return <ListLayout posts={filteredPosts} title={title} />;
 }
