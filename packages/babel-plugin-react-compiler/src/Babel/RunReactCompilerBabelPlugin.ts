cd9a3b6c534ca8b9d3caf30b5694554615c08cbd
hyunjin <jin.2@kakaoent.com>
2025-07-05 19:56:14 +0900

    build react compiler

--- packages/babel-plugin-react-compiler/src/Babel/RunReactCompilerBabelPlugin.ts
+++ packages/babel-plugin-react-compiler/src/Babel/RunReactCompilerBabelPlugin.ts
@@ -3,18 +3,33 @@ import { transformFromAstSync } from '@babel/core'
 import * as BabelParser from '@babel/parser'
 import invariant from 'invariant'
 import type { PluginOptions } from '../EntryPoint'
+import BabelPluginReactCompiler from './BabelPlugin'
 
 export function runBabelPluginReactCompiler(
   text: string,
   file: string,
   language: 'flow' | 'typescript',
-
   options: Partial<PluginOptions> | null,
   includeAst: boolean = false,
-) {
+): BabelCore.BabelFileResult {
   const ast = BabelParser.parse(text, {
     sourceFilename: file,
     plugins: [language, 'jsx'],
     sourceType: 'module',
   })
+
+  const result = transformFromAstSync(ast as any, text, {
+    ast: includeAst,
+    filename: file,
+    highlightCode: false,
+    retainLines: true,
+    plugins: [[BabelPluginReactCompiler, options], 'babel-plugin-fbt', 'babel-plugin-fbt-runtime'],
+    sourceType: 'module',
+    configFile: false,
+    babelrc: false,
+  })
+
+  invariant(result?.code != null, `Expected BabelPluginReactForget to codegen successfully, got: ${result}`)
+
+  return result
 }
