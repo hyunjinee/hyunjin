db671916ab9a19e7663d604e3465ada061b785a2
hyunjin <jin.2@kakaoent.com>
2025-10-17 17:12:51 +0900

    start resume 2024 project

---
+++ services/resume2024/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: '이현진 2024',
+  description: '이현진의 2024 이력서',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
+      </head>
+      <body className="antialiased">{children}</body>
+    </html>
+  )
+}
