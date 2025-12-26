e79b0f395881137b589e6fe7e8d70364ddb7d664
hyunjin <jin.2@kakaoent.com>
2025-12-26 18:18:38 +0900

    Add Pretendard font integration and update global styles for improved typography
    - Introduced a new Pretendard font module for better font management.
    - Updated globals.css to define font families and apply the Pretendard font.
    - Modified layout.tsx to utilize the new font variable and class for consistent styling.
    - Added PretendardVariable.woff2 font file for local usage.

--- services/resume2024/src/app/layout.tsx
+++ services/resume2024/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { pretendard } from '@/fonts/pretendard'
 
 export const metadata: Metadata = {
   title: '이현진 2024',
@@ -15,16 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <head>
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
-        />
-      </head>
-      <body className="antialiased">{children}</body>
+    <html lang="ko" className={pretendard.variable}>
+      <body className={`antialiased ${pretendard.className}`}>{children}</body>
     </html>
   )
 }
