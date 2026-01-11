bd9da2bc74399dcaa804513871a4ff467bf1e466
hyunjin <jin.2@kakaoent.com>
2026-01-11 13:07:05 +0900

    style: clean up import statements in rollup-config index.js for consistency

--- configs/rollup-config/src/index.js
+++ configs/rollup-config/src/index.js
@@ -1,6 +1,6 @@
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
 
-console.log('Hello, world!');
+console.log('Hello, world!')
