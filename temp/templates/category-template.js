3e6ce13a0600d7e56771635a68503756ec370401
hyunjin <jin.2@kakaoent.com>
2026-02-10 07:00:22 +0900

    refactor: streamline imports and remove unnecessary semicolons for cleaner code

--- temp/templates/category-template.js
+++ temp/templates/category-template.js
@@ -1,28 +1,27 @@
-import React, { useMemo, useCallback } from 'react';
-import { navigate } from 'gatsby';
-
-import Layout from '../layout';
-import Seo from '../components/seo';
-import Post from '../models/post';
-import CategoryPageHeader from '../components/category-page-header';
-import PostTabs from '../components/post-tabs';
+import { navigate } from 'gatsby'
+import { useCallback, useMemo } from 'react'
+import CategoryPageHeader from '../components/category-page-header'
+import PostTabs from '../components/post-tabs'
+import Seo from '../components/seo'
+import Layout from '../layout'
+import Post from '../models/post'
 
 function CategoryTemplate({ pageContext }) {
-  const { edges, currentCategory } = pageContext;
-  const { categories } = pageContext;
+  const { edges, currentCategory } = pageContext
+  const { categories } = pageContext
   const currentTabIndex = useMemo(
     () => categories.findIndex((category) => category === currentCategory),
     [categories, currentCategory],
-  );
-  const posts = edges.map(({ node }) => new Post(node));
+  )
+  const posts = edges.map(({ node }) => new Post(node))
 
   const onTabIndexChange = useCallback(
     (e, value) => {
-      if (value === 0) return navigate(`/posts`);
-      navigate(`/posts/${categories[value]}`);
+      if (value === 0) return navigate(`/posts`)
+      navigate(`/posts/${categories[value]}`)
     },
     [categories],
-  );
+  )
 
   return (
     <Layout>
@@ -30,7 +29,7 @@ function CategoryTemplate({ pageContext }) {
       <CategoryPageHeader title={categories[currentTabIndex]} subtitle={`${posts.length} posts`} />
       <PostTabs tabIndex={currentTabIndex} onChange={onTabIndexChange} tabs={categories} posts={posts} />
     </Layout>
-  );
+  )
 }
 
-export default CategoryTemplate;
+export default CategoryTemplate
